3705436af2ae849ed1c4c1e17589b03fed24810a
Guillaume Favelier <guillaume.favelier@gmail.com>
2021-07-02 15:21:09 +0200

    Update

---
+++ examples/plot_pyvista.py
@@ -0,0 +1,8 @@
+"""
+Example 1
+=========
+"""
+import pyvista as pv
+p = pv.Plotter()
+p.add_mesh(pv.Cone())
+p.show()
--- examples/plot_pyvistaqt.py
+++ examples/plot_pyvistaqt.py
@@ -4,6 +4,5 @@ Example 1
 """
 import pyvista as pv
 from pyvistaqt import BackgroundPlotter
-
 p = BackgroundPlotter()
 p.add_mesh(pv.Cone())
