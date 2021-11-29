54ccfc8f68d3cf6e8fabe18dcbfd1bbf166f0377
Guillaume Favelier <guillaume.favelier@gmail.com>
2021-11-29 17:06:06 +0100

    try with simple example

--- examples/plot_pyvistaqt.py
+++ examples/plot_pyvistaqt.py
@@ -7,12 +7,6 @@ from pyvistaqt import BackgroundPlotter
 pyvista.OFF_SCREEN = True
 pyvista.BUILDING_GALLERY = True
 
-p = BackgroundPlotter(line_smoothing=True)
-p.add_mesh(pyvista.Cone(), smooth_shading=True)
-p.enable_anti_aliasing()
-p.enable_depth_peeling()
+p = BackgroundPlotter()
+p.add_mesh(pyvista.Cone())
 p.show()
-# p.update()
-p._render()
-# p.app.processEvents()
-p.app_window.show()
