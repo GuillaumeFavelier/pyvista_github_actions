3927a5483ffb50497ccdd6f7f88e176369d6bf26
Guillaume Favelier <guillaume.favelier@gmail.com>
2021-11-29 18:16:20 +0100

    try mne now

--- examples/plot_mne.py
+++ examples/plot_mne.py
@@ -4,7 +4,8 @@ Example MNE
 """
 import mne
 import pyvista
-pyvista.OFF_SCREEN = False
+pyvista.OFF_SCREEN = True
+pyvista.BUILDING_GALLERY = True
 
 fname = mne.datasets.sample.data_path() + '/MEG/sample/sample_audvis_raw.fif'
 raw = mne.io.read_raw_fif(fname)
