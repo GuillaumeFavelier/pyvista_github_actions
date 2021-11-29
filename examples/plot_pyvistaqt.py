8c781a6d5447de57fac028408476c2c1b8eed540
Guillaume Favelier <guillaume.favelier@gmail.com>
2021-11-29 17:41:47 +0100

    enable line smoothing

--- examples/plot_pyvistaqt.py
+++ examples/plot_pyvistaqt.py
@@ -7,7 +7,7 @@ from pyvistaqt import BackgroundPlotter
 pyvista.OFF_SCREEN = True
 pyvista.BUILDING_GALLERY = True
 
-p = BackgroundPlotter()
+p = BackgroundPlotter(line_smoothing=True)
 p.add_mesh(pyvista.Cone(), smooth_shading=True)
 p.enable_anti_aliasing()
 p.enable_depth_peeling()
