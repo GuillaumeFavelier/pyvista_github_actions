960d8be145444ce62b29ec1e3b98697cb0214a56
Guillaume Favelier <guillaume.favelier@gmail.com>
2021-11-30 13:09:03 +0100

    add plot_mne_stc

---
+++ examples/plot_mne_stc.py
@@ -0,0 +1,16 @@
+import os
+import mne
+from mne.datasets import sample
+import pyvista
+pyvista.OFF_SCREEN = True
+pyvista.BUILDING_GALLERY = True
+
+data_path = sample.data_path()
+sample_dir = os.path.join(data_path, 'MEG', 'sample')
+subjects_dir = os.path.join(data_path, 'subjects')
+fname_stc = os.path.join(sample_dir, 'sample_audvis-meg')
+stc = mne.read_source_estimate(fname_stc, subject='sample')
+initial_time = 0.13
+brain = stc.plot(subjects_dir=subjects_dir, initial_time=initial_time,
+                 clim=dict(kind='value', lims=[3, 6, 9]),
+                 size=600, background="white", hemi='lh')
