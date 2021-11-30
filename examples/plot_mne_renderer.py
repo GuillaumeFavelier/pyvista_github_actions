45ff5b23ffea36e7a32ad8eaf94752fa8b4c8ef7
Guillaume Favelier <guillaume.favelier@gmail.com>
2021-11-30 11:20:35 +0100

    try again

--- examples/plot_mne_renderer.py
+++ examples/plot_mne_renderer.py
@@ -2,41 +2,37 @@
 Example MNE/Renderer
 ====================
 """
-import numpy as np
-import pyvista
 from mne.viz.backends import renderer
+import pyvista
 pyvista.OFF_SCREEN = True
 pyvista.BUILDING_GALLERY = True
 
-win_size = (600, 600)
-win_color = 'grey'
-
-tet_size = 1.0
-tet_x = np.array([0, tet_size, 0, 0])
-tet_y = np.array([0, 0, tet_size, 0])
-tet_z = np.array([0, 0, 0, tet_size])
-tet_indices = np.array([[0, 1, 2],
-                        [0, 1, 3],
-                        [0, 2, 3],
-                        [1, 2, 3]])
-tet_color = 'white'
-
 # init scene
 rend = renderer.create_3d_figure(
-    size=win_size,
-    bgcolor=win_color,
-    smooth_shading=True,
+    size=(600, 600),
+    bgcolor='grey',
+    smooth_shading=False,
     scene=False,
 )
-for interaction in ('terrain', 'trackball'):
-    rend.set_interaction(interaction)
+
+rend.plotter.add_mesh(pyvista.Cone())
 
 # use mesh
-mesh_data = rend.mesh(
-    x=tet_x,
-    y=tet_y,
-    z=tet_z,
-    triangles=tet_indices,
-    color=tet_color,
-)
+# import numpy as np
+# tet_size = 1.0
+# tet_x = np.array([0, tet_size, 0, 0])
+# tet_y = np.array([0, 0, tet_size, 0])
+# tet_z = np.array([0, 0, 0, tet_size])
+# tet_indices = np.array([[0, 1, 2],
+#                         [0, 1, 3],
+#                         [0, 2, 3],
+#                         [1, 2, 3]])
+# tet_color = 'white'
+# mesh_data = rend.mesh(
+#     x=tet_x,
+#     y=tet_y,
+#     z=tet_z,
+#     triangles=tet_indices,
+#     color=tet_color,
+# )
 rend.show()
